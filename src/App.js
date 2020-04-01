7c479e86c49bb0db0f04c3e5bc08ed9e0c034bf0
Erwin Debusschere <erwincdl@gmail.com>
2020-03-31 22:27:43 -0300

    Wip

--- src/App.js
+++ src/App.js
@@ -1,78 +1,95 @@
 import React, { useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
-const Speaker = ({ isMain, name }) => {
+const Speaker = ({ speaker: { name } }) => {
     return (
-        <div className={isMain ? '' : 'Speaker-queued'}>
-            <img src={logo} className={isMain ? 'Speaker-logo-main' : 'Speaker-logo-queued'} alt="logo"/>
-            <p>
-                {name}
-            </p>
+        <div className="card">
+            <div className="speaker-logo"/>
+            <div className="speaker-name">{name}</div>
         </div>
     );
 };
 
-const Queued = ({ queued }) => {
-    const empty = queued.length === 0;
-    if (empty) {
-        return <div/>;
-    } else {
-        return <div className="Queue-queued">
-            <h2 className="UpNext">Siguen</h2>
-            <TransitionGroup>
-                {queued.map(({ name }, index) =>
-                    <CSSTransition
-                        in
-                        appear
-                        unmountOnExit
-                        classNames="speaker"
-                        key={index}
-                        timeout={{ appear: 300, exit: 200 }}
-                    >
-                        <Speaker name={name}/>
-                    </CSSTransition>)
-                }
-            </TransitionGroup>
-        </div>;
-    }
+const FABs = ({ onPlus, onMinus }) => {
+    return <div className="fab">
+        <div className="circle" onClick={onPlus}>
+            <Emoji aria="hand" emoji="✋"/>
+        </div>
+        <div className="circle" onClick={onMinus}>
+            <Emoji aria="no-women" emoji="🙅‍♀"/>
+        </div>
+    </div>;
 };
-
-function Queue({ members }) {
-    const [main, ...queued] = members;
-    const empty = members.length === 0;
-    return (
-        <>
-            {empty ? null :
-                <div className="Queue-body">
-                    <div className="Queue-main">
-                        <Speaker isMain name={main.name}/>
-                    </div>
-                    <Queued queued={queued}/>
-                </div>
-            }
-        </>
-    );
-}
 
 const App = () => {
     const participants = [
         { name: 'Erwin' },
-        { name: 'Erwin' }
+        { name: 'Nico' },
+        { name: 'Ernesto' },
+        { name: 'Agustin' },
+        { name: 'Fernando' },
+        { name: 'Fabian' },
+        { name: 'Virginia' },
+        { name: 'Marcelo' },
+        { name: 'Horacio' },
+        { name: 'Alberto' },
+        { name: 'Marianela' },
+        { name: 'Hernan Rogelio Arias ApellidoLargo' }
     ];
     const [queue, setQueue] = useState(participants);
     return (
-        <div className="App">
-            <Queue members={queue}/>
-            <div className="Actions">
-                <div className="circle" onClick={() => setQueue([...queue, { name: 'Ernesto' }])}>
-                    <Emoji aria="hand" emoji="✋"/>
-                </div>
-                <div className="circle" onClick={() => setQueue(queue.slice(1))}>
-                    <Emoji aria="no-women" emoji="🙅‍♀"/>
+        <div className="app">
+            <div className="header">
+                N-Q-it!
+            </div>
+            <div className="main">
+                <h2>Speaker</h2>
+                <div className="main-speaker">
+                    {queue[0] ?
+                        <Speaker
+                            speaker={queue[0]}
+                        /> : null
+                    }
                 </div>
             </div>
+            <div className="up-next">
+                <h2 className="lighter">Up next</h2>
+                <div className="up-next-speakers">
+                    {
+                        queue.slice(1, 4).map((speaker, index) =>
+                            <Speaker
+                                speaker={speaker}
+                                key={index}
+                            />)
+                    }
+                </div>
+            </div>
+            <div className="queued">
+                <h2 className="lighter">Queued</h2>
+                <div className="queued-speakers">
+                    {
+                        queue.slice(4).map((speaker, index) => {
+                            return <div key={index} className="queued-speaker">
+                                <Speaker
+                                    speaker={speaker}
+                                />
+                            </div>;
+                        })
+                    }
+                </div>
+            </div>
+            <FABs
+                onPlus={() => setQueue([...queue, { name: 'ernesto' }])}
+                onMinus={() => setQueue(queue.slice(1))}
+            />
+            {/*<Queue members={queue}/>*/}
+            {/*<div className="Actions">*/}
+            {/*    <div className="circle" onClick={() => setQueue([...queue, { name: 'Ernesto' }])}>*/}
+
+            {/*    </div>*/}
+            {/*    <div className="circle" onClick={() => setQueue(queue.slice(1))}>*/}
+            {/*        <Emoji aria="no-women" emoji="🙅‍♀"/>*/}
+            {/*    </div>*/}
+            {/*</div>*/}
         </div>
     );
 };
